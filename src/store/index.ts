35d1a323e4c84bbe2b30bb80a787451f0b661e23
ka1mak <dev1.00.ab@gmail.com>
2023-07-03 16:03:06 +0600

    removed extra slices

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import bucketSlice from './reducers/bucketSlice'
 import appSlice from './reducers/appSlice'
-import marketSlice from './reducers/marketSlice'
 
 const rootReducer = combineReducers({
-  bucketSlice,
   appSlice,
-  marketSlice,
 })
 
 export const setupStore = () => {
--- src/store/reducers/appSlice.ts
+++ src/store/reducers/appSlice.ts
@@ -1,19 +1,29 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 interface AppState {
+  bucket: number[]
   theme: string
 }
 
 const initialState: AppState = {
-  theme: 'dark'
+  bucket: [],
+  theme: 'dark',
 }
 
 export const appSlice = createSlice({
-  name: 'app',
+  name: 'app-state',
   initialState,
   reducers: {
-    changeTheme(state, action: PayloadAction<string>) {
+    changeTheme (state, action: PayloadAction<string>) {
       state.theme = action.payload
+    },
+
+    addToBucket (state, action: PayloadAction<number>) {
+      state.bucket = [...state.bucket, action.payload]
+    },
+
+    removeFromBucket (state, action: PayloadAction<number>) {
+      state.bucket = state.bucket.filter(id => id !== action.payload)
     },
   }
 })
