cae98d42094c08e2d9dfd58ae069e100f49133f5
ka1mak <dev1.00.ab@gmail.com>
2023-07-06 16:44:57 +0600

    did adding to the bucket, and the bucket page

---
+++ src/pages/Bucket/pages/List.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Bucket } from '..'
+
+import CardSkeleton from 'components/Skeleton/Card'
+import PageTitle from 'components/PageTitle'
+import Card from 'components/Card'
+
+export const List = () => {
+  const { goods, isLoading } = Bucket.Hooks.List.use()
+
+  const loader = () => goods.map(({ id }) => <CardSkeleton key={id} />)
+  console.log(goods)
+
+  return (
+    <>
+      <PageTitle title="Корзина" path="Bucket /" />
+
+      <div className="flex flex-wrap justify-center p-1 tablet:p-4 laptop:px-10">
+        {
+          isLoading
+            ? loader()
+            : goods.map(good => <Card good={good} key={good.id} />)
+        }
+      </div>
+    </>
+  )
+}
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Sidebar from 'layouts/Sidebar'
 import { Market } from './Market'
+import { Bucket } from './Bucket'
 
 const MainLayout = () => {
   return (
     <Sidebar>
       <Routes>
         <Route path='/market/*' element={<Market.Pages />} />
+        <Route path='/bucket/*' element={<Bucket.Pages />} />
       </Routes>
     </Sidebar>
   )
