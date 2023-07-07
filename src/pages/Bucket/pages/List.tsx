7bafb0b041e9c827997f6e27787723db0edff698
ka1mak <dev1.00.ab@gmail.com>
2023-07-07 20:39:45 +0600

    checking if the bucket is empty

--- src/pages/Bucket/pages/List.tsx
+++ src/pages/Bucket/pages/List.tsx
@@ -1,32 +1,43 @@
 import React from 'react'
 import { useAppSelector } from 'hooks/redux'
-
+import { NavLink } from 'react-router-dom'
 import { Bucket } from '..'
 
-import {
-  Skeleton,
-  PageTitle,
-  Grid,
-  Card,
-} from 'components'
+import { Skeleton, PageTitle, Grid, Card } from 'components'
 
 export const List = () => {
   const { goods, isLoading } = Bucket.Hooks.List.use()
   const bucket = useAppSelector(state => state.appSlice.bucket)
 
-  const loader = () => [...new Array(bucket.length)].map((_, i) => <Skeleton key={i} />)
+  const loader = () => (
+    <Grid>
+      {[...new Array(bucket.length)].map((_, i) => <Skeleton key={i} />)}
+    </Grid>
+  )
+
+  const renderGoods = () => (
+    <Grid>
+      {goods.map(good => <Card good={good} key={good.id} />)}
+    </Grid>
+  )
+
+  const renderEmptyState = () => (
+    <div className="text-center text-xl p-10">
+      Список корзины пока пуст!<br />
+      Вернитесь в
+      <NavLink
+        to="/market"
+        className="border-b border-slate-500 cursor-pointer"
+      > каталог </NavLink>
+      и добавьте товар
+    </div>
+  )
 
   return (
     <div className="min-h-screen">
       <PageTitle title="Корзина" path="Bucket /" />
 
-      <Grid>
-        {
-          isLoading
-            ? loader()
-            : goods.map(good => <Card good={good} key={good.id} />)
-        }
-      </Grid>
+      {isLoading ? loader() : (goods.length > 0 ? renderGoods() : renderEmptyState())}
     </div>
   )
 }
